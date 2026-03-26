5477bfaadbf25ad44e324fafd617b48e0d9c0f46
Chaitanya Kasved <kasvedc@gmail.com>
2026-03-26 11:45:39 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace learning
+{
+	class comment
+	{
+		static void Main(string[] args)
+		{
+			// This is a Single Line Comment.
+			/* This is a Multi-Line Comments
+			You can see i'm Typing on another Line.
+			*/
+		}
+	}
+}
